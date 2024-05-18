3b7ea975aa321cb93946d60c2c3809db3dbcc7c0
Brad Siegel <blsiege@gmail.com>
2024-05-17 23:06:46 -0400

    Initial Commit

---
+++ AQIViewer/AQIViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AQIViewerApp.swift
+//  AQIViewer
+//
+//  Created by Brad Siegel on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct AQIViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
